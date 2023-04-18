9c6c4c8ec361454f43d7cf19201a6f6879377d45
Vikash <vikash00mot@gmail.com>
2023-04-18 11:56:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new typed("multiple-text",
+{
+    string:["Data Analyst" , "Fullstack Developer" , "Motivator" , "Educator"],
+    typespeed : 100,
+    backspeed : 100,
+    backDelay:1000,
+    loop: true
+
+})
